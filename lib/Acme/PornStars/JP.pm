6817373439ab596699c96f3ebe1af265879e0a0c
wata <nagasawa@junkapp.com>
2011-07-17 07:11:56 +0900

    use Coro

--- lib/Acme/PornStars/JP.pm
+++ lib/Acme/PornStars/JP.pm
@@ -6,6 +6,8 @@ use utf8;
 use Lingua::JA::Moji qw/kana2romaji/;
 use URI;
 use XML::Simple;
+use Coro;
+use Coro::Select;
 use Furl::HTTP;
 use Cache::FileCache;
 
@@ -14,80 +16,103 @@ sub new {
     my $opt   = @_;
     my $cache = Cache::FileCache->new($opt || {
         cache_root         => '/tmp',
-        namespace          => 'pornstars',
+        namespace          => 'Acme-PornStars-JP',
         default_expires_in => '7d',
     });
     bless { cache => $cache }, $class;
 }
 
 sub get {
-    my ( $self, $initial ) = @_;
-    my $url = URI->new('http://ja.wikipedia.org/wiki/特別:データ書き出し/AV女優一覧_' . $initial . '行');
+    my $self = shift;
     my $cache = $self->{cache};
 
     my $ret;
-    unless ( $ret = $cache->get($url) ) {
-        my $furl = Furl::HTTP->new( agent => 'Mozilla/5.0' );
-        my (undef, $code, undef, undef, $body) = $furl->get($url);
-        die $code unless $code eq 200;
-
-        my $content = XMLin($body)->{page}{revision}{text}{content};
-        my @actress;
-        foreach my $line ( split( /\n/, $content ) ) {
-            next unless $line =~ /^\*\s\[\[/;
-            if ( $line =~ /\[\[([^\[\]]+)\]\][（(]([^()（）]+)[)）](.*)/ ) {
-                my ( $actress_info, $tmp ) = ( { name => $1, yomi => $2 }, $3 );
-                $actress_info->{name} =~ s/.*\|//;
-                $actress_info->{engname} =
-                    kana2romaji( $actress_info->{yomi},
-                                 { style => 'passport', ve_type => 'none' } );
-                $actress_info->{year} =
-                    $tmp =~ /[（(](?:\[\[)?(\d{4})\s*年(?:\]\])?[)）]/ ? $1 : '';
-                $actress_info->{initial} = $initial;
-                push @actress, $actress_info;
-            }
+    unless ( $ret = $cache->get('pornstars') ) {
+        my @coros;
+        foreach my $i ( qw/ あ か さ た な は ま や ら・わ / ) {
+            push @coros, async {
+                return _initial_get->($i);
+            };
         }
-        $cache->set($url, \@actress);
-        $ret = \@actress;
+        my @pornstars;
+        foreach my $coro (@coros) {
+            my $pornstars = $coro->join;
+            next unless $pornstars;
+            push @pornstars, @{ $pornstars };
+        }
+
+        my $ret = \@pornstars;
+        $cache->set('pornstars', $ret);
+        $self->{pornstars} = $ret;
     }
-    $self->{actress} = $ret;
+
     return $ret;
+}
+
+sub _initial_get {
+    my $initial = shift;
+    my $url = URI->new('http://ja.wikipedia.org/wiki/特別:データ書き出し/AV女優一覧_' . $initial . '行');
+
+    my $furl = Furl::HTTP->new( agent => 'Mozilla/5.0' );
+    my (undef, $code, undef, undef, $body) = $furl->get($url);
+    die $code unless $code eq 200;
+
+    my $content = XMLin($body)->{page}{revision}{text}{content};
+    my @pornstars;
+    foreach my $line ( split( /\n/, $content ) ) {
+        next unless $line =~ /^\*\s\[\[/;
+        if ( $line =~ /\[\[([^\[\]]+)\]\][（(]([^()（）]+)[)）](.*)/ ) {
+            my ( $star_info, $tmp ) = ( { name => $1, yomi => $2 }, $3 );
+            $star_info->{name} =~ s/.*\|//;
+            $star_info->{engname} =
+                kana2romaji( $star_info->{yomi},
+                    { style => 'passport', ve_type => 'none' } );
+            $star_info->{year} =
+                $tmp =~ /[（(](?:\[\[)?(\d{4})\s*年(?:\]\])?[)）]/ ? $1 : '';
+            $star_info->{initial} = $initial;
+            push @pornstars, $star_info;
+        }
+    }
+    return \@pornstars;
 }
 
 sub year {
     my ( $self, $year ) = @_;
-    my @temp;
-    foreach my $actress ( @{ $self->{actress} } ) {
-        push @temp, $actress if $actress->{year} eq $year;
+    my @tmp;
+    foreach my $star ( @{ $self->{pornstars} } ) {
+        push @tmp, $star if $star->{year} eq $year;
     }
-    return \@temp;
+    return \@tmp;
 }
 
 sub name {
     my ( $self, $name ) = @_;
-    foreach my $actress ( @{ $self->{actress} } ) {
-        return $actress if $actress->{name} eq $name;
+    my @tmp;
+    foreach my $star ( @{ $self->{pornstars} } ) {
+        push @tmp, $star if $star->{name} eq $name;
     }
-    return;
+    return \@tmp;
 }
 
 sub yomi {
     my ( $self, $yomi ) = @_;
-    foreach my $actress ( @{ $self->{actress} } ) {
-        my $y = $actress->{yomi};
+    my @tmp;
+    foreach my $star ( @{ $self->{pornstars} } ) {
+        my $y = $star->{yomi};
         $y =~ s/[ 　]+//g;
         $yomi =~ s/[ 　]+//g;
-        return $actress if $y eq $yomi;
+        push @tmp, $star if $y eq $yomi;
     }
-    return;
+    return \@tmp;
 }
 
 sub engname {
     my ( $self, $engname) = @_;
-    foreach my $actress ( @{ $self->{actress} } ) {
-        return $actress if $actress->{engname} eq $engname;
+    my @tmp;
+    foreach my $star ( @{ $self->{pornstars} } ) {
+        push @tmp, $star if $star->{engname} eq $engname;
     }
-    return;
+    return \@tmp;
 }
 
 1;
@@ -100,7 +125,6 @@ Acme::PornStars::JP -
 =head1 SYNOPSIS
 
   use Acme::PornStars::JP;
-  use utf8;
   use YAML;
   my $pornstars = Acme::PornStars::JP->new(
       {
@@ -109,7 +133,7 @@ Acme::PornStars::JP -
           'default_expires_in' => '30d',
       },
   );
-  $pornstars->get('あ');
+  $pornstars->get;
   my $starlist = $pornstars->year('2000');
   print YAML::Dump($starlist);
 
