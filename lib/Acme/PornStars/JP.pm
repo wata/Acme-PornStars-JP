445c589bf59c616f1c3b7adb74227576ed2cc4c6
wata <nagasawa@junkapp.com>
2011-07-16 21:12:27 +0900

    initial commit

---
+++ lib/Acme/PornStars/JP.pm
@@ -0,0 +1,32 @@
+package Acme::PornStars::JP;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::PornStars::JP -
+
+=head1 SYNOPSIS
+
+  use Acme::PornStars::JP;
+
+=head1 DESCRIPTION
+
+Acme::PornStars::JP is
+
+=head1 AUTHOR
+
+Wataru Nagasawa E<lt>nagasawa {at} junkapp.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ xt/01_podspell.t
@@ -0,0 +1,10 @@
+use Test::More;
+eval q{ use Test::Spelling };
+plan skip_all => "Test::Spelling is not installed." if $@;
+add_stopwords(map { split /[\s\:\-]/ } <DATA>);
+$ENV{LANG} = 'C';
+all_pod_files_spelling_ok('lib');
+__DATA__
+Wataru Nagasawa
+nagasawa {at} junkapp.com
+Acme::PornStars::JP
